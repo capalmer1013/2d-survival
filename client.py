4fc599bc3bfc696546cc1701440c7f7c3b08d560
Chris Palmer <capalmer1013@gmail.com>
2022-06-01 22:29:31 -0400

    connection established

--- client.py
+++ client.py
@@ -2,22 +2,49 @@ from pygase import Client
 import time
 import main
 import gameObjects
+import asyncio
+import socketio
 
-client = Client()
-client.connect_in_thread(port=8080, hostname="localhost")
-game = main.App(headless=False, networked=True)
-game.scene = main.SCENE_PLAY
-game.WORLD_HEIGHT = game.SCREEN_HEIGHT
-game.WORLD_WIDTH = game.SCREEN_WIDTH
+sio = socketio.AsyncClient()
 
-def update_play_scene(self):
-    with client.access_game_state() as game_state:
-        for each in game_state.__dict__:
-            print(each)
-            print(type(each))
-            print(game_state.__dict__[each])
-            tmpObj = gameObjects.BaseGameObject.deserialize(each, game.player, game)
-            if tmpObj not in game.gameObjects:
-                game.gameObjects.append(tmpObj)
+@sio.event
+async def connect():
+    print('connection established')
 
-game.start()
+@sio.event
+async def my_message(data):
+    print('message received with ', data)
+    await sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+async def disconnect():
+    print('disconnected from server')
+
+async def main():
+    await sio.connect('http://localhost:5000')
+    await sio.wait()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
+
+# client = Client()
+# client.connect_in_thread(port=8080, hostname="localhost")
+# game = main.App(headless=False, networked=True)
+# game.scene = main.SCENE_PLAY
+# game.WORLD_HEIGHT = game.SCREEN_HEIGHT
+# game.WORLD_WIDTH = game.SCREEN_WIDTH
+#
+# def update_play_scene(self):
+#     with client.access_game_state() as game_state:
+#         for each in game_state.__dict__:
+#             print(each)
+#             print(type(each))
+#             print(game_state.__dict__[each])
+#             tmpObj = gameObjects.BaseGameObject.deserialize(each, game.player, game)
+#             if tmpObj not in game.gameObjects:
+#                 game.gameObjects.append(tmpObj)
+#
+# game.start()
--- server.py
+++ server.py
@@ -3,43 +3,68 @@ import pickle
 import time
 from pygase import GameState, Backend
 import main
+import eventlet
+import socketio
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+def my_message(sid, data):
+    print('message ', data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
 
-game_cycle_time = 1/30
-
-game = main.App(headless=True)
-game.WORLD_HEIGHT = game.SCREEN_HEIGHT
-game.WORLD_WIDTH = game.SCREEN_WIDTH
-game.initWorld(0.05)
-game.scene = main.SCENE_PLAY
-
-total_dt = 0.0
-
-stale_game_state = {str(x.id): x.serialize() for x in game.gameObjects}
-
-initial_game_state = GameState(**stale_game_state)
-
-count = 0
 
 
-def time_step(game_state, dt):
-    global total_dt
-    global stale_game_state
 
-    global count
-    count += 1
-    total_dt += dt
-    if total_dt > game_cycle_time:
-        total_dt = 0.0
-        game.update()
-        stale_game_state.update({str(x.id): x.serialize() for x in game.gameObjects if x.moved})
+# game_cycle_time = 1/30
+#
+# game = main.App(headless=True)
+# game.WORLD_HEIGHT = game.SCREEN_HEIGHT
+# game.WORLD_WIDTH = game.SCREEN_WIDTH
+# game.initWorld(0.05)
+# game.scene = main.SCENE_PLAY
+#
+# total_dt = 0.0
+#
+# stale_game_state = {str(x.id): x.serialize() for x in game.gameObjects}
+#
+# initial_game_state = GameState(**stale_game_state)
+#
+# count = 0
 
-    return stale_game_state
 
-
-for each in initial_game_state.__dict__:
-    print(each)
-print(len(initial_game_state.__dict__))
-
-backend = Backend(initial_game_state, time_step)
-backend.run('localhost', 8080)
+# def time_step(game_state, dt):
+#     global total_dt
+#     global stale_game_state
+#
+#     global count
+#     count += 1
+#     total_dt += dt
+#     if total_dt > game_cycle_time:
+#         total_dt = 0.0
+#         game.update()
+#         stale_game_state.update({str(x.id): x.serialize() for x in game.gameObjects if x.moved})
+#
+#     return stale_game_state
+#
+#
+# for each in initial_game_state.__dict__:
+#     print(each)
+# print(len(initial_game_state.__dict__))
+#
+# backend = Backend(initial_game_state, time_step)
+# backend.run('localhost', 8080)
